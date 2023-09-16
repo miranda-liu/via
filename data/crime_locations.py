f2584128b3172657b48d41cdfbc40734d0abdc1c
Remi Kuba <remi.g.kuba@gmail.com>
2023-09-16 18:46:09 -0400

    Merge offense code files

--- data/crime_locations.py
+++ data/crime_locations.py
@@ -1,19 +1,29 @@
 import csv
-import math
+import offense_codes as offenses
 
-csv_name = '2023BostonCrime.csv'
-with open(csv_name, 'r', newline = '') as file:
-    reader = csv.reader(file)
-    locations = {}
-    for i, row in enumerate(reader):
-        if i == 0:
-            continue
-        location, offense_code = row[16], int(row[1])
-        if not location:
-            continue
-        coord = location[1:-1].split(", ")
-        location = round(float(coord[0]),5), round(float(coord[1]),5)
-        crimes = locations.get(location,{})
-        crimes[offense_code] = crimes.get(offense_code,0) + 1
-        locations[location] = crimes
-    print(locations)
+def main():
+    csv_name = 'data/2023BostonCrime.csv'
+    with open(csv_name, 'r', newline = '') as file:
+        bad_offenses = offenses.get_bad_offense_codes()
+        reader = csv.reader(file)
+        locations,  = {}
+        for i, row in enumerate(reader):
+            if i == 0:
+                continue
+
+            location = get_location(row[16])
+            offense_code = int(row[1])
+            if offense_code not in bad_offenses:
+                continue
+            crimes = locations.get(location,{})
+            crimes[offense_code] = crimes.get(offense_code,0) + 1
+            locations[location] = crimes
+        print(locations)
+
+def get_location(loc_string):
+    # Some reports don't include locations
+    if not loc_string:
+        return ""
+    coord = loc_string[1:-1].split(", ")
+    location = round(float(coord[0]),6), round(float(coord[1]),6)
+    return location
--- data/offense_codes.py
+++ data/offense_codes.py
@@ -1,17 +1,37 @@
+import pandas as pd
 import csv
 
-csv_name = '2023BostonCrime.csv'
-with open(csv_name, 'r', newline = '') as file:
-    reader = csv.reader(file)
-    offense_codes = {}
-    for i,row in enumerate(reader):
-        if not i:
-            continue
-        code, description = int(row[1]), row[3]
-        if not description:
-            continue
-        if code not in offense_codes:
-            offense_codes[code] = description
-for k,v in offense_codes.items():
-    print(k,v)
-print(len(offense_codes))
+def get_bad_offense_codes():
+    df = pd.read_excel('data/all_offense_codes.xlsx')
+    codelist = df['CODE'].tolist()
+    offense = df['NAME'].tolist()
+    code_list_new = []
+    offenselist_new = []
+
+    for elt in offense:
+        if pd.isna(elt) == False:
+            offenselist_new.append(elt)
+
+    for elt in codelist:
+        if pd.isna(elt) == False:
+            code_list_new.append(elt)
+
+    # convert the list of key-value pairs to a dictionary
+    bad_offenses = dict(zip(code_list_new, offenselist_new))
+    return bad_offenses
+
+def get_crimes():
+    bad_offenses = get_bad_offense_codes()
+    csv_name = 'data/2023BostonCrime.csv'
+    with open(csv_name, 'r', newline = '') as file:
+        reader = csv.reader(file)
+        crimes = {}
+        for i,row in enumerate(reader):
+            if not i:
+                continue
+            code, description = int(row[1]), row[3]
+            if not description or code not in bad_offenses:
+                continue
+            if code not in crimes:
+                crimes[code] = description
+    return crimes
