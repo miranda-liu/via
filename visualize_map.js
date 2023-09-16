8b8d5dca23fdebe7b20f3e3e518549ec66fe7d3e
miranda-liu <47536718+miranda-liu@users.noreply.github.com>
2023-09-16 15:33:21 -0400

    edit

---
+++ visualize_map.js
@@ -0,0 +1,14 @@
+import mapboxgl from 'mapbox-gl';
+
+// mapboxgl.accessToken = '<pk.eyJ1Ijoicmt1YmEiLCJhIjoiY2xtbDZtbnBtMDg3MzJrbW9jNTJqMzVibiJ9.1G7FCXA5MdikERXLIAuJLQ>';
+// const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v12', // style URL
+//     center: [-74.5, 40], // starting position [lng, lat]
+//     zoom: 9 // starting zoom
+// });
+
+map.on('load', function () {
+    map.resize();
+    // dynamic map sizing
+});
